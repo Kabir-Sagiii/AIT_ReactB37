1d546b10c85a2dab6352ca0fb00138ad825c0bdc
Kabir <sagithedeveloper@gmail.com>
2025-07-14 19:57:08 +0530

    Added the code

---
+++ ReactJS/S27_State/src/App.jsx
@@ -0,0 +1,14 @@
+import Wish from "./components/state-concept/Wish";
+import Image from "./components/state-concept/Image";
+import DataRender from "./components/state-concept/DataRender";
+function App() {
+  return (
+    <div>
+      <DataRender />
+      {/* <Image />
+      <Wish /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/S27_State/src/components/relations/b/B.jsx
@@ -0,0 +1,9 @@
+function B() {
+  return (
+    <div style={{ backgroundColor: "lightsalmon", padding: "30px" }}>
+      <h1>B Component</h1>
+    </div>
+  );
+}
+
+export default B;
